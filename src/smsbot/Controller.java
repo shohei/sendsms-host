fa5c397c714ee24ad4a66ecc1dc1a9395e594d39
Shohei Aoki <shoaok@gmail.com>
2016-03-11 20:22:38 +0000

    deal with xls and xlsx

--- src/smsbot/Controller.java
+++ src/smsbot/Controller.java
@@ -7,6 +7,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -57,7 +63,12 @@ public class Controller implements Initializable {
         if (file != null) {
             fileLoadLabel.setText("File location:");
             filePathLabel.setText(file.toPath().toString());
-            readExcel(file);
+            String ext = FilenameUtils.getExtension(file.getAbsolutePath());
+            if(ext.equals("xlsx")||ext.equals("XLSX")){
+                readXLSX(file);
+            }else if(ext.equals("xls")||ext.equals("XLS")){
+                readXLS(file);
+            }
         }
     }
 
@@ -70,7 +81,15 @@ public class Controller implements Initializable {
         this.serialComboBox.getItems().removeAll(this.serialComboBox.getItems());
     }
 
-    public void readExcel(File file) {
+    public void parsePhoneNumber(){
+        //TODO:
+    }
+
+    public void validatePhoneNumber(){
+        //TODO:
+    }
+
+    public void readXLSX(File file) {
         FileInputStream inputStream;
         try {
             inputStream = new FileInputStream(file);
@@ -110,9 +129,47 @@ public class Controller implements Initializable {
         } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
-
     }
 
+    public void readXLS(File file){
+        try {
+            POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
+            HSSFWorkbook wb = new HSSFWorkbook(fs);
+            HSSFSheet sheet = wb.getSheetAt(0);
+            HSSFRow row;
+            HSSFCell cell;
+
+            int rows; // No of rows
+            rows = sheet.getPhysicalNumberOfRows();
+
+            int cols = 0; // No of columns
+            int tmp = 0;
+
+            // This trick ensures that we get the data properly even if it doesn't start from first few rows
+            for(int i = 0; i < 10 || i < rows; i++) {
+                row = sheet.getRow(i);
+                if(row != null) {
+                    tmp = sheet.getRow(i).getPhysicalNumberOfCells();
+                    if(tmp > cols) cols = tmp;
+                }
+            }
+
+            for(int r = 0; r < rows; r++) {
+                row = sheet.getRow(r);
+                if(row != null) {
+                    for(int c = 0; c < cols; c++) {
+                        cell = row.getCell((short)c);
+                        if(cell != null) {
+                            // Your code here
+                            System.out.println(cell.toString());
+                        }
+                    }
+                }
+            }
+        } catch(Exception ioe) {
+            ioe.printStackTrace();
+        }
+    }
 
     public void initComboBox() {
         java.util.Enumeration<CommPortIdentifier> portEnum = CommPortIdentifier.getPortIdentifiers();
